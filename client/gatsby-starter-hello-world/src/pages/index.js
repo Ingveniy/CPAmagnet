bc739c6bbff930b70ea574ce8dc2ba269aba6d9b
Eugene.Melentyev <eugene.melentyev@mobilitygroup.net>
2019-03-30 22:42:22 +0300

    gatsby

---
+++ client/gatsby-starter-hello-world/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Header from "../components/header";
+import Sidebar from "../components/sidebar";
+
+export default () => (
+  <div>
+    <Header />
+    <Sidebar />
+    <h1>Index page</h1>
+  </div>
+);
